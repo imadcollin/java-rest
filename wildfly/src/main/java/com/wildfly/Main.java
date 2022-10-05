8783f2031a05524ea31c279b5d4cfdb64f7e6c24
imadcollin <imad.collin@gmail.com>
2022-10-05 22:31:26 +0200

    Add wildfly example

---
+++ wildfly/src/main/java/com/wildfly/Main.java
@@ -0,0 +1,19 @@
+package com.wildfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class Main extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(com.wildfly.Main.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
+}
